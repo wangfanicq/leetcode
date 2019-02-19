685900da14b64911d7f4b52d40e1f74ebfce7aa6
wang <wangfanicq@163.com>
2019-02-19 10:49:35 +0000

    given target,get index

---
+++ twosum.py
@@ -0,0 +1,9 @@
+def twosum(nums,target):
+    dic={}
+    for i in range(len(nums)):
+        if target-nums[i] in dic.keys:
+            return i,dic[target-nums[i]]
+        else:
+            dic[nums[i]]=i
+
+            
