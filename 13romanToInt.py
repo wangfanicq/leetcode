ceebc0986d1dbcb374f7ef212d61007166b3aae4
wangfanicq <wangfanicq@163.com>
2019-02-20 11:02:39 +0800

    Create 13romanToInt.py

---
+++ 13romanToInt.py
@@ -0,0 +1,15 @@
+def romanToInt(s):
+	'''
+	type s : str
+	type return : int
+	'''
+	dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+	ans=0
+	for i in range(len(s)-1):
+		if dic[s[i]]<dic[s[i+1]]:
+			ans-=dic[s[i]]
+		else:
+			ans+=dic[s[i]]
+	ans+=dic[s[len(s)-1]]
+	return ans
+	
